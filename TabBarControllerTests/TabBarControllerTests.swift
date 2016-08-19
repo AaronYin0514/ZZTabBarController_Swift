94f37c87ab48ccf7deaea745ccd49337186c7e8f
Aaron <562540603@qq.com>
2016-08-19 16:03:46 +0800

    Initial Commit

---
+++ TabBarControllerTests/TabBarControllerTests.swift
@@ -0,0 +1,36 @@
+//
+//  TabBarControllerTests.swift
+//  TabBarControllerTests
+//
+//  Created by Aaron on 16/8/19.
+//  Copyright © 2016年 Aaron. All rights reserved.
+//
+
+import XCTest
+@testable import TabBarController
+
+class TabBarControllerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
