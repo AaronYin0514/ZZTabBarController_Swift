7f20afe2d0c52a2e1fe686b94eac7602402f6b3e
Aaron <562540603@qq.com>
2016-09-12 19:45:32 +0800

    设置badge值

--- TabBarController/PlanViewController.swift
+++ TabBarController/PlanViewController.swift
@@ -8,28 +8,29 @@
 
 import UIKit
 
-class PlanViewController: UIViewController {
+class PlanViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.view.backgroundColor = UIColor.greenColor()
+        let tableView = UITableView(frame: self.view.bounds)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.registerClass(NSClassFromString("UITableViewCell"), forCellReuseIdentifier: "Id")
+        self.view.addSubview(tableView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 35
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Id")
+        cell?.textLabel?.text = "第\(indexPath.row)行"
+        return cell!
     }
-    */
+    // MARK: - UITableViewDelegate
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.zz_tabBarItem.badgeValue = String(indexPath.row)
+    }
 
 }
